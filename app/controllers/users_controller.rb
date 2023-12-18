259ebc9be04182170b4d5f4e22ed308d06cbbaec
Anna N Helms <unicornbuddha@gmail.com>
2023-12-17 16:10:17 -0800

    add update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,14 @@ class UsersController < ApplicationController
         @user = User.find_by(id: params[:id])
         render :show
     end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        @user.update(
+            name: params[:name] || @user.name,
+            email: params[:email] || @user.email,
+            image_url: params[:image_url] || @user.image_url,
+        )
+        render :show
+    end
 end
