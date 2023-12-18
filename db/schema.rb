19f072d8dace8b695642e7ebac64d2980824ce41
Anna N Helms <unicornbuddha@gmail.com>
2023-12-17 21:20:31 -0800

    add model with seed for initial data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_18_041636) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_18_051526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_18_041636) do
     t.string "name"
     t.string "latitude"
     t.string "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "image_url"
+    t.integer "coffee_shops_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 # Review.create(coffee_shops_id: 1, trip_id: 1, description: "Lovely atmosphere! This is one of my all time favorite places to grab a coffee and knock out a study session.", rating: 4, user_id: 1 )
 # Review.create(coffee_shops_id: 2, trip_id: 1, description: "I love the coffee roasts from this place. The Bitches Brew (a nod to Miles Davis) is my go to everyday coffee.", rating: 4, user_id: 2 )
 
-Trip.create(user_id: 1, coffee_shops_id: 1)
-Trip.create(user_id: 2, coffee_shops_id: 2)
+# Trip.create(user_id: 1, coffee_shops_id: 1)
+# Trip.create(user_id: 2, coffee_shops_id: 2)
+
+Image.create(image_url: "https://koreatownlosangeles.com/wp-content/uploads/2021/10/coffeesignal.la_241757864_258112406183583_2943665939706211594_n-1.jpg", coffee_shops_id: 1)
+Image.create(image_url: "https://www.sprudgemaps.com/wp-content/uploads/2023/01/larch1.jpg", coffee_shops_id: 2)
