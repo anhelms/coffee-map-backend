d78827d73bc12c9cd402365f2500000e5dd53d63
Anna N Helms <unicornbuddha@gmail.com>
2024-01-10 19:39:53 -0800

    fixes cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173", "https://coffee-maps-app.onrender.com/"
+    origins "example.com", "localhost:5173", "coffee-maps-app.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
